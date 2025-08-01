997003a7b60601a99dc4e589e9c353f38aefc391
Shivam Tripathi <97305325+shivam0975@users.noreply.github.com>
2025-08-02 00:16:43 +0530

    Update index.js

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -9,24 +9,24 @@ async function handler(req, res) {
     const products = await Product.find({});
     return res.status(200).json(products);
   }
+
   else if (req.method === 'POST') {
     if (!req.user.isAdmin) {
       return res.status(403).json({ message: 'Admin resource. Access denied.' });
     }
 
     const { name, description, price, image, countInStock } = req.body;
-
-    if(!name || !description || price == null || !image || countInStock == null) {
+    if (!name || !description || price == null || !image || countInStock == null) {
       return res.status(400).json({ message: 'All fields required' });
     }
 
     const product = await Product.create({ name, description, price, image, countInStock });
-
     return res.status(201).json(product);
   }
+
   else {
     res.setHeader('Allow', ['GET', 'POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
 

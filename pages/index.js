26799460cc9f0c05fb7841db702aa8715097b82b
Shivam Tripathi <97305325+shivam0975@users.noreply.github.com>
2025-08-03 18:17:29 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,10 @@ export default function Home() {
 
   return (
     <>
+      <Navbar />
       <Head>
         <title>Shivam's E-Shop</title>
       </Head>
-      <Navbar />
       <main className="max-w-6xl mx-auto p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map((product) => (
           <div key={product._id} className="bg-white p-4 rounded shadow">
